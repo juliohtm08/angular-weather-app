a018a17f04c2084776beceb1a10ae2f9793e2e56
juliohtm08 <jhtomoyuki345@gmail.com>
2025-06-11 19:07:42 -0300

    Criando serviço de previsão do tempo

---
+++ src/app/modules/weather/service/weather.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  private apiKey = 'a8b978d60630d3d08b8a39e8b1e8d8e0';
+
+  constructor(private http: HttpClient) {}
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+      {}
+    );
+  }
+}
